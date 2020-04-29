d71e8b6dedc03eec8b94f156d29631d9cdc7f895
Jonathan Koh <jontkoh@gmail.com>
2020-04-28 19:54:18 -0700

    added rate limit w/ max of 50 and 10 seconds

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import 'dotenv/config';
 import server from './src';
 
-
 const port = process.env.PORT;
 
 try {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import fastify from 'fastify';
 import helmet from 'fastify-helmet';
+import rateLimit from 'fastify-rate-limit';
 import {logger} from '../logs';
+import {setRateLimit} from './rate-limit';
 
 const server = fastify({ logger: logger });
 server.register(helmet);
+server.register(
+  rateLimit, 
+  setRateLimit(process.env.TIME_WINDOW, process.env.MAX_LIMIT)
+);
 
 server.get('/', async () => {
   const result = { hello: 'world'};
