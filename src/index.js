7f67f6439dc07e0a5a9077b5ec4637ee7c932e33
Darren Chan <darren@darchan0319@gmail.com>
2020-05-26 19:11:13 -0700

    Edited Docker-compose file for exposing the postgres database

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import helmet from 'fastify-helmet';
 import rateLimit from 'fastify-rate-limit';
 import {logger} from '../logs';
 import {setRateLimit} from './config/rate-limit';
+import {setDatabaseConnections} from './config/database';
 import { routes } from './routes';
+
 
 const server = fastify({ logger: logger });
 server.register(helmet);
 server.register(
   rateLimit, 
+  
   setRateLimit(process.env.TIME_WINDOW, process.env.MAX_LIMIT)
 );
+setDatabaseConnections(server);
 
 server.register(routes);
 
