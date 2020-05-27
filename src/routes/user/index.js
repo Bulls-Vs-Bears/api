3eab04ecf9f39f58598bd195741f394368b6fd53
Darren Chan <darren@darchan0319@gmail.com>
2020-05-26 19:11:13 -0700

    Edited Docker-compose file for exposing the postgres database

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -1,13 +1,20 @@
-
 export async function userRoutes(server) {
   server.get('/user', getUserHandler);
-  server.post('/user', postUserHandler);
+  server.post('/user', await createUserHandler);
+
+  async function createUserHandler(req) {
+    console.log("we are hereeeee");
+    console.log(req.body); 
+    const client = await server.pg.pgWriter.connect();
+    await client.query(
+      'INSERT INTO user (user_name, user_password, user_email) \
+      VALUES ()'
+    );
+  }
 }
 
 async function getUserHandler() {
   return { hello: 'me'};
 }
 
-async function postUserHandler(req) {
-  return req.body
-}
+
