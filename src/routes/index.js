c85451ae5c2c9445accce48d2fb3a2bea0ea89a2
Darren Chan <darren@darchan0319@gmail.com>
2020-06-05 13:44:04 -0700

    Added Comments, status codes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,13 @@
 import { userRoutes } from './user';
 
+const { HTTP_SUCCESS, } = process.env; 
+
 export async function routes(server) {
-  server.register(userRoutes, {prefix: '/api/v1'})
+  server.register(userRoutes, { prefix: '/api/v1' });
+
+  server.get('/', async (_, res) => {
+    //TODO: change this endpoint to display swagger.
+    const result = { hello: 'world'};
+    return res.code(HTTP_SUCCESS).send(result);
+  });
 }
--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -1,20 +1,33 @@
+const { HTTP_SUCCESS, HTTP_CREATED, HTTP_ERROR, } = process.env; 
+
 export async function userRoutes(server) {
   server.get('/user', getUserHandler);
   server.post('/user', await createUserHandler);
 
-  async function createUserHandler(req) {
-    console.log("we are hereeeee");
-    console.log(req.body); 
-    const client = await server.pg.pgWriter.connect();
-    await client.query(
-      'INSERT INTO user (user_name, user_password, user_email) \
-      VALUES ()'
-    );
+  async function getUserHandler(req, res) {
+    // TODO: Finish this after /user POST
+    try {
+      res.code(HTTP_SUCCESS).send({ req });
+    } catch (err) {
+      server.log.error(err);
+      res.code(HTTP_ERROR).send(err);
+    }
+  }
+
+  async function createUserHandler(req, res) {
+    //! NOTE: This is not done yet.
+    try {
+      // const client = await server.pg.pgWriter.connect();
+      // const { username, password, email } = req.body;
+      
+      // await client.query(
+      //   'INSERT INTO user (user_name, user_password, user_email) \
+      //   VALUES ()', [username, password, email]
+      // );
+      res.code(HTTP_CREATED).send({ req });
+    } catch (err) {
+      server.log.error(err);
+      res.code(HTTP_ERROR).send(err);
+    }
   }
 }
-
-async function getUserHandler() {
-  return { hello: 'me'};
-}
-
-
