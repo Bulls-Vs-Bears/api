b5b018b89f8d9d2496fdb2c8e4e82fae2cba1a80
Darren Chan <darren@darchan0319@gmail.com>
2020-06-05 13:44:04 -0700

    Added Comments, status codes

--- src/index.js
+++ src/index.js
@@ -1,26 +1,18 @@
 import fastify from 'fastify';
 import helmet from 'fastify-helmet';
 import rateLimit from 'fastify-rate-limit';
-import {logger} from '../logs';
-import {setRateLimit} from './config/rate-limit';
-import {setDatabaseConnections} from './config/database';
+import { logger } from '../logs';
+import { setRateLimit } from './config/rate-limit';
+import { setDatabaseConnections } from './config/database';
 import { routes } from './routes';
-
 
 const server = fastify({ logger: logger });
 server.register(helmet);
 server.register(
   rateLimit, 
-  
   setRateLimit(process.env.TIME_WINDOW, process.env.MAX_LIMIT)
 );
 setDatabaseConnections(server);
-
 server.register(routes);
-
-server.get('/', async () => {
-  const result = { hello: 'world'};
-  return result;
-});
 
 export default server;
