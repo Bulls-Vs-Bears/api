246dc16f0391d40b8c12d76c5552880b01d194be
philipbaledia <philipbaledia@gmail.com>
2020-08-02 11:10:21 -0700

    updated response.js

--- src/util/response.js
+++ src/util/response.js
@@ -1,7 +1,22 @@
-export function responseMessage(type, outcome, data, token){
-    let message = "User registered successfully";
+export function responseMessage(type, success, data, token){
+    let message = "Data registered successfully";
+    if (success == true){
+        switch(type){
+            case 'get':
+                message = 'Data found';
+                break;
+            case 'put':
+                message = 'Data updated successfully';
+                break;
+            case 'delete':
+                message = 'Data deleted successfully';
+                break;
+        }
+    } else {
+        message = "Invalid Data";
+    }
     const response = {
-        success: outcome, 
+        success: success, 
         message: message, 
         data: data,
         token: token 
