a820556af8d8f7beadc20c897d379d3c78411964
Jonathan Koh <jontkoh@gmail.com>
2020-08-06 17:45:52 -0700

    hashing is working, however it errors when using salt rounds as an ENV

--- src/config/database/postgres/hash-password.js
+++ src/config/database/postgres/hash-password.js
@@ -1,12 +1,44 @@
-//! From the debugging I have been doing, after spinning up the containers,
-//! I'm unable to access the '/'. 
-
+// import { SALT_ROUNDS, } from 'config';
 import bcrypt from 'bcrypt';
 
+
+// Hashing password to store into Postgres
 export async function hashPassword(password){
-  // keeping salt round as local var for testing, will change to env later
   const saltRounds = 8;
-  bcrypt.hash(password, saltRounds).then(function(hash) {
-    return hash;
-  });
+  const hashedPassword = await new Promise((resolve, reject) => {
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+      if(err){
+        reject(err);
+      }
+      resolve(hash);
+    })
+  })
+
+  //----------------------------------------------------------------
+  // !! Currently having a problem with setting salt rounds as ENV 
+  // !! it will error with saying data and salt arguments required
+  // const hashedPassword = await new Promise((resolve, reject) => {
+  //   bcrypt.genSalt(SALT_ROUNDS, function(err, salt) {
+  //     bcrypt.hash(password, salt, function(err, hash) {
+  //       if (err) {
+  //         reject(err);
+  //       }
+  //       resolve(hash);
+  //     });
+  //   });
+  // });
+  return hashedPassword;
+}
+
+// Check and confirm if password and hash is true
+export async function checkHash(password, hashedPassword) {
+  const result = await new Promise((resolve, reject) => {
+    bcrypt.compare(password, hashedPassword, function(err, result) {
+      if(err){
+        reject(err);
+      }
+      resolve(result);
+    })
+  })
+  return result;
 }
--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -1,5 +1,5 @@
 import { HTTP_SUCCESS, HTTP_CREATED, HTTP_ERROR, } from 'config'; 
-import { hashPassword } from '../../config/database/postgres/hash-password';
+import { hashPassword, checkHash } from '../../config/database/postgres/hash-password';
 
 export async function userRoutes(server) {
   server.get('/user', getUserHandler);
@@ -8,7 +8,7 @@ export async function userRoutes(server) {
   async function getUserHandler(req, res) {
     // TODO: Finish this after /user POST
     try {
-      res.code(HTTP_SUCCESS).send({ req });
+      res.code(HTTP_SUCCESS).send({ });
     } catch (err) {
       server.log.error(err);
       res.code(HTTP_ERROR).send(err);
@@ -20,20 +20,30 @@ export async function userRoutes(server) {
       const client = await server.pg.pgWriter.connect();
       const { username, password, email } = req.body;
       
-      // hashing password using bcrypt
+      // hash password using bcrypt
       const hashedPassword = await hashPassword(password);
 
       await client.query(
         'INSERT INTO bvb_accounts.user (user_name, user_password, user_email) VALUES ($1, $2, $3)',
         [username, hashedPassword, email]
       );
+
+      // query for hashed password
+      const passwordToCheck = await client.query(
+        'SELECT user_password FROM bvb_accounts.user WHERE user_name = $1 ', 
+        [username]
+      );
+      
+      // !! passwordToCheck was saved as a json obj so had to get password by doing the following: 
+      const isHashed = await checkHash(password, passwordToCheck.rows[0].user_password);
        //! NOTE: This is not done yet, we want to 
        //! fix the response objects to have some sort of standard.
       const response = {
-        success: true, 
-        message: "User registered successfully", 
-        data: {}, 
-        token: {},
+        "success": true, 
+        "message": "User registered successfully", 
+        "checkHash": isHashed,
+        "data": {}, 
+        "token": {},
       };
 
       res.code(HTTP_CREATED).send(response);
