7e0da0b58f0ad7b14b798c07302304f1fb9c75f1
Jonathan Koh <jontkoh@gmail.com>
2020-08-20 10:56:01 -0700

    removed checkhash from createUserHandler(), and added error throws to hash-password functions.

--- src/utils/hash-password.js
+++ src/utils/hash-password.js
@@ -3,21 +3,23 @@ import bcrypt from 'bcrypt';
 
 
 // Hashing password to store into Postgres
-export async function hashPassword(password) {
+export async function createHashedPassword(password) {
   const salt = await bcrypt.genSalt(parseInt(SALT_ROUNDS, 10));
   const hashedPassword = await bcrypt.hash(password, salt);
+  if (hashedPassword == Error) {
+    throw hashedPassword;
+  }
   return hashedPassword;
 }
 
 // Check and confirm if password and hash is true
-export async function checkHash(password, hashedPassword) {
-  const result = await new Promise((resolve, reject) => {
-    bcrypt.compare(password, hashedPassword, function(err, result) {
-      if(err){
-        reject(err);
-      }
-      resolve(result);
-    })
-  })
+// disabling eslint for now bc of checkHash not being used
+/* eslint-disable */
+export async function doCheckHash(password, hashedPassword) { 
+  const result = await bcrypt.compare(password, hashedPassword)
+  if (result == Error) {
+    throw result;
+  }
   return result;
 }
+/* eslint-enable */
